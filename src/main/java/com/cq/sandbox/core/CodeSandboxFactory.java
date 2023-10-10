3132ea76769b00597513f9bb3e50ad68735e3ca4
asce <2497409117@qq.com>
2023-10-10 09:12:05 +0800

    沙箱更新

--- src/main/java/com/cq/sandbox/core/CodeSandboxFactory.java
+++ src/main/java/com/cq/sandbox/core/CodeSandboxFactory.java
@@ -2,6 +2,9 @@ package com.cq.sandbox.core;
 
 import com.cq.sandbox.model.enums.QuestionSubmitLanguageEnum;
 
+/**
+ * 根据语言类型获得对应的代码沙箱
+ */
 public class CodeSandboxFactory {
     public static CodeSandboxTemplate getInstance(QuestionSubmitLanguageEnum language) {
         switch (language.getValue()) {
